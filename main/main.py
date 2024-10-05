ccef1e04800715b44e9a5ee4106b3ffe66051501
Henry Rothenberg <hsr205@aol.com>
2024-10-04 20:23:37 -0400

    Alter main method

--- main/main.py
+++ main/main.py
@@ -1,5 +1,22 @@
+import pandas as pd
+from logger import Logger
+from data_loader import DataLoader
+
+logger: Logger = Logger().get_logger()
+
+pd.set_option('display.max_columns', None)
+
+
 def main() -> int:
-    print(f"Hello from {__name__}")
+    data_loader: DataLoader = DataLoader()
+
+    file_path: str = ""
+    file_name: str = "fraudulent_ecommerce_transaction_data.csv"
+
+    fraud_data_frame: pd.DataFrame = data_loader.get_data_frame_from_zip_file(file_path=file_path, file_name=file_name)
+
+    logger.info(f"{fraud_data_frame.head()}")
+
     return 0
 
 

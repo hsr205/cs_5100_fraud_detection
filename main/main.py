0a1dcc7e989a0954d1e08408f1997462be04b090
Henry Rothenberg <hsr205@aol.com>
2024-10-09 15:41:48 -0400

    Add FraudDataset logic

--- main/main.py
+++ main/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from logger import Logger
+
 from custom_data_loader import CustomDataLoader
+from fraud_data import FraudDataset
+from logger import Logger
 
 logger: Logger = Logger().get_logger()
 
@@ -13,7 +15,13 @@ def main() -> int:
     file_path: str = ""
     file_name: str = "synthetic_financial_datasets_log.csv"
 
-    fraud_data_frame: pd.DataFrame = data_loader.get_data_frame_from_zip_file(file_path=file_path, file_name=file_name)
+    fraud_data: FraudDataset = FraudDataset(data_loader=data_loader,
+                                            file_path=file_path,
+                                            file_name=file_name,
+                                            transform_to_tensor=None,
+                                            target_transform=None)
+
+    fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path,file_name=file_name)
 
     logger.info(f"{fraud_data_frame.head()}")
 

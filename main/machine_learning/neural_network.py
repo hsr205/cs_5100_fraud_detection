3c5d712720b829de794f0147a7c9b3ddc45abdf8
Henry Rothenberg <hsr205@aol.com>
2024-10-28 18:27:06 -0400

    Add NeuralNetwork class

---
+++ main/machine_learning/neural_network.py
@@ -0,0 +1,20 @@
+import torch
+from logger import Logger
+from torch import nn
+
+logger: Logger = Logger().get_logger()
+
+"""
+We define our neural network by subclassing nn.Module
+"""
+class NeuralNetwork(nn.Module):
+
+    def __init__(self):
+        device = (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+        logger.info(f"Using {device} device")

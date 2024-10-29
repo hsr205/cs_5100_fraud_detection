7de6314b2557b6c111ffc079ad86099b6429939d
Henry Rothenberg <hsr205@aol.com>
2024-10-29 09:20:27 -0400

    Add demonstration logic

--- main/main.py
+++ main/main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 from data.custom_data_loader import CustomDataLoader
+from data.fraud_data import FraudDataset
 from logger import Logger
-from machine_learning.k_means_learning import KMeansLearning
-from static.constants import Constants
+from machine_learning.neural_network import DataPreprocessor
 
 logger: Logger = Logger().get_logger()
 
@@ -13,36 +13,21 @@ pd.set_option('display.max_columns', None)
 def main() -> int:
     data_loader: CustomDataLoader = CustomDataLoader()
 
+    # Add local file path to .zip file
     file_path: str = ""
     file_name: str = "synthetic_financial_datasets_log.csv"
 
-    k_means: KMeansLearning = KMeansLearning(data_loader=data_loader,
-                                             file_path=file_path,
-                                             file_name=file_name,
-                                            #  transform_to_tensor=None,
-                                            #  target_transform=None, 
-                                             k=3)
+    fraud_data: FraudDataset = FraudDataset(data_loader=data_loader,
+                                            file_path=file_path,
+                                            file_name=file_name,
+                                            transform_to_tensor=None,
+                                            target_transform=None)
 
-    k_means.display_sample_of_data_points(num_data_points=1000, x_axis_str=Constants.BEFORE_TRANSACTION_BALANCE, y_axis_str=Constants.NEW_TRANSACTION_BALANCE)
+    fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path,
+                                                                                         file_name=file_name)
+    data_preprocessor: DataPreprocessor = DataPreprocessor(fraud_data_frame=fraud_data_frame)
 
-    # fraud_data: FraudDataset = FraudDataset(data_loader=data_loader,
-    #                                         file_path=file_path,
-    #                                         file_name=file_name,
-    #                                         transform_to_tensor=None,
-    #                                         target_transform=None)
-    #
-    # fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path,
-    #                                                                                      file_name=file_name)
-
-    # logger.info(f"Unique Name Original Account Numbers: {len(fraud_data_frame['nameOrig'].unique())}") # 6,353,307
-    # logger.info(f"Unique Name Destination Account Numbers: {len(fraud_data_frame['nameDest'].unique())}") # 2,722,362
-
-    # logger.info(f"Num of rows indicating fraud: {fraud_data_frame[fraud_data_frame['isFraud'] == 1].shape[0]}") # 8,213
-    # logger.info(f"Num of rows NOT indicating fraud: {fraud_data_frame[fraud_data_frame['isFraud'] == 0].shape[0]}") # 6,354,407
-
-    # logger.info(f"{fraud_data_frame.head()}")
-
-    k_means.execute_clustering()
+    data_preprocessor.preprocess_type_column_data()
 
     return 0
 

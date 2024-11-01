78c65db005de77aa3980dffa3b2eebe3cf6a55c4
Henry Rothenberg <hsr205@aol.com>
2024-11-01 09:56:42 -0400

    Alter / add execution logic

--- main/main.py
+++ main/main.py
@@ -1,35 +1,33 @@
-import pandas as pd
-
 import os
+from pathlib import Path
+
+import pandas as pd
 from data.custom_data_loader import CustomDataLoader
 from data.fraud_data import FraudDataset
-from logger import Logger
-from machine_learning.neural_network import DataPreprocessor
 from machine_learning.neural_network import Model
 
-logger: Logger = Logger().get_logger()
-
-pd.set_option('display.max_columns', None)
 
 def main() -> int:
     data_loader: CustomDataLoader = CustomDataLoader()
 
-    # Include local file path to zip file
-    file_path: str = os.path.join(os.getcwd(),"main","data","fraud_detection_data_set","synthetic_financial_datasets_log.zip")
-    file_name: str = "Synthetic_Financial_datasets_log.csv"
+    file_path_to_data: str = str(
+        Path.cwd() / "data" / "fraud_detection_data_set" / "synthetic_financial_datasets_log.zip")
+    file_name: str = "synthetic_financial_datasets_log.csv"
 
     fraud_data: FraudDataset = FraudDataset(data_loader=data_loader,
-                                            file_path=file_path,
+                                            file_path=file_path_to_data,
                                             file_name=file_name,
                                             transform_to_tensor=None,
                                             target_transform=None)
 
-    fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path,
+    fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path_to_data,
                                                                                          file_name=file_name)
 
-    model:Model = Model(fraud_data_frame=fraud_data_frame)
-    model.train_neural_network()
-    os.system("tensorboard --logdir=runs")
+    model: Model = Model(fraud_data_frame=fraud_data_frame)
+    epoch_loss_list: list[float] = model.train_neural_network()
+    model.write_results(epoch_loss_list=epoch_loss_list)
+    model.save_model_state()
+    model.launch_tensor_board()
     return 0
 
 

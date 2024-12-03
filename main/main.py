5ec7f225e5a3c96bce192e202ca4bc33621331b7
Henry Rothenberg <hsr205@aol.com>
2024-12-03 16:21:16 -0500

    WIP

--- main/main.py
+++ main/main.py
@@ -25,23 +25,6 @@ def main() -> int:
 
     k_means.display_sample_of_data_points(num_data_points=1000, x_axis_str=Constants.BEFORE_TRANSACTION_BALANCE, y_axis_str=Constants.NEW_TRANSACTION_BALANCE)
 
-    # fraud_data: FraudDataset = FraudDataset(data_loader=data_loader,
-    #                                         file_path=file_path,
-    #                                         file_name=file_name,
-    #                                         transform_to_tensor=None,
-    #                                         target_transform=None)
-    #
-    # fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path,
-    #                                                                                      file_name=file_name)
-
-    # logger.info(f"Unique Name Original Account Numbers: {len(fraud_data_frame['nameOrig'].unique())}") # 6,353,307
-    # logger.info(f"Unique Name Destination Account Numbers: {len(fraud_data_frame['nameDest'].unique())}") # 2,722,362
-
-    # logger.info(f"Num of rows indicating fraud: {fraud_data_frame[fraud_data_frame['isFraud'] == 1].shape[0]}") # 8,213
-    # logger.info(f"Num of rows NOT indicating fraud: {fraud_data_frame[fraud_data_frame['isFraud'] == 0].shape[0]}") # 6,354,407
-
-    # logger.info(f"{fraud_data_frame.head()}")
-
     k_means.execute_clustering()
 
     return 0

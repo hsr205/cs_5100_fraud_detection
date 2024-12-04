13dc69cc286d99a1fe2a5f3d184029829ee43315
Henry Rothenberg <hsr205@aol.com>
2024-12-04 16:46:48 -0500

    Add execution logic

--- main/main.py
+++ main/main.py
@@ -4,9 +4,7 @@ import pandas as pd
 
 from data.custom_data_loader import CustomDataLoader
 from data.fraud_data import FraudDataset
-from machine_learning.anomaly_detection import IFModel
-from machine_learning.neural_network import Accuracy
-from machine_learning.neural_network import Model
+from main.execution import Execution
 
 
 def main() -> int:
@@ -23,14 +21,10 @@ def main() -> int:
 
     fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path_to_data,
                                                                                          file_name=file_name)
-    isolation_forest: IFModel = IFModel(fraud_data_frame=fraud_data_frame)
-    isolation_forest.detect(16000)
+    execution_object: Execution = Execution()
 
-    model: Model = Model(fraud_data_frame=fraud_data_frame)
-    epoch_loss_list: list[list[float]] = model.train_neural_network(epochs=20)
-    model.write_results(epoch_loss_list=epoch_loss_list)
-    model.save_model_state()
-    accuracy_results: Accuracy = model.test_neural_network()
-    model.display_testing_results(accuracy_obj=accuracy_results)
-    model.launch_tensor_board()
+    execution_object.execute_isolation_forest(fraud_data_frame=fraud_data_frame)
+    execution_object.execute_k_means(fraud_data_frame=fraud_data_frame)
+    execution_object.execute_neural_network(fraud_data_frame=fraud_data_frame)
+
     return 0

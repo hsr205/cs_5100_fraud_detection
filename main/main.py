f03c98afc66c497580fb4d48042acc57dbf747f5
Henry Rothenberg <hsr205@aol.com>
2024-12-04 17:02:12 -0500

    Reformat code

--- main/main.py
+++ main/main.py
@@ -8,6 +8,7 @@ from main.execution import Execution
 
 
 def main() -> int:
+    execution_object: Execution = Execution()
     data_loader: CustomDataLoader = CustomDataLoader()
     file_path_to_data: str = str(
         Path.cwd() / "data" / "fraud_detection_data_set" / "synthetic_financial_datasets_log.zip")
@@ -21,7 +22,6 @@ def main() -> int:
 
     fraud_data_frame: pd.DataFrame = fraud_data.data_loader.get_data_frame_from_zip_file(file_path=file_path_to_data,
                                                                                          file_name=file_name)
-    execution_object: Execution = Execution()
 
     execution_object.execute_isolation_forest(fraud_data_frame=fraud_data_frame)
     execution_object.execute_k_means(fraud_data_frame=fraud_data_frame)
